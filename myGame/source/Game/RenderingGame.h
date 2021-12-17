7627403e0c27efac6829ffd008ed912d5062d28b
mdeubler04 <mdeubler04@gmail.com>
2021-12-17 00:26:24 +0000

    mdeubler first project commit

---
+++ myGame/source/Game/RenderingGame.h
@@ -0,0 +1,76 @@
+#pragma once
+
+
+#include "Game.h"
+
+using namespace Library;
+
+namespace Library
+{
+    class FirstPersonCamera;
+	class RenderStateHelper;
+
+	class Keyboard;
+	class Mouse;
+	class FpsComponent;
+
+}
+
+namespace DirectX
+{
+	class SpriteBatch;
+	class SpriteFont;
+}
+
+
+namespace Rendering
+{
+    //class TriangleDemo;
+	//class ModelFromFile;
+	//class ObjectDiffuseLight;
+	class ShadowMappingDemo;
+
+    class RenderingGame : public Game
+    {
+    public:
+        RenderingGame(HINSTANCE instance, const std::wstring& windowClass, const std::wstring& windowTitle, int showCommand);
+        ~RenderingGame();
+
+        virtual void Initialize() override;		
+        virtual void Update(const GameTime& gameTime) override;
+        virtual void Draw(const GameTime& gameTime) override;
+
+
+	protected:
+        virtual void Shutdown() override;
+
+    private:
+		static const XMFLOAT4 BackgroundColor;
+        FirstPersonCamera * mCamera;
+        //TriangleDemo* mDemo;
+
+		//Define member variables for Keyboard and mouse
+		LPDIRECTINPUT8 mDirectInput;
+
+		Keyboard* mKeyboard;
+		Mouse*    mMouse;
+		//ModelFromFile* mModel1;
+		//ModelFromFile* mModel2;
+	
+		int mScore;
+		SpriteBatch* mSpriteBatch;
+		SpriteFont* mSpriteFont;
+
+		FpsComponent* mFpsComponent;
+		RenderStateHelper* mRenderStateHelper;
+		ShadowMappingDemo* mShadowMappingDemo;
+
+
+		//ObjectDiffuseLight* mObjectDiffuseLight;
+
+		//void Pick(int sx, int sy, ModelFromFile*);
+
+	
+
+    };
+}
